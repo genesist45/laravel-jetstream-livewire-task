b9188339632bbb753d0a7e75c2fc2b68e8346afd
Genesis Clark <genesisclark79o@gmail.com>
2025-08-28 08:21:31 +0800

    Updated

--- app/Models/StudentModel.php
+++ app/Models/StudentModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class studentModel extends Model
+class StudentModel extends Model
 {
     protected $table = 't_students';
     protected $fillable = ['first_name', 'last_name', 'middle_name', 'gender', 'dob', 'age'];
